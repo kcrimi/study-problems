2c91a08fa30ce47508490bc82b9d6e9542544a8c
Kevin Crimi <kevincrimi@gmail.com>
2024-02-17 08:12:24 -0800

    Update Solution.ts

--- Java/src/Codility/PrimeAndCompositeNumbers/Solution.ts
+++ Java/src/Codility/PrimeAndCompositeNumbers/Solution.ts
@@ -1,6 +1,4 @@
-// you can write to stdout for debugging purposes, e.g.
-// console.log('this is a debug message');
-
+//Times out on large ones
 function solution(N: number): number {
     if (N == 1) return 1
     var d = 1
@@ -10,4 +8,27 @@ function solution(N: number): number {
         d++
     }
     return factors
+}
+
+//sieve 
+function solution(N: number): number {
+    if (N == 1) return 1
+    var d = 1
+    var factors = new Set<number>()
+    while (d <= Math.sqrt(N) ) {
+        if (!factors.has(d) && N % d == 0) {
+            factors.add(d)
+            factors.add(N/d)
+            let i = 2
+            while (d * i <= Math.sqrt(N)){
+                if (!factors.has(d*i) && N % (d*i) == 0) {
+                    factors.add(d)
+                    factors.add(N/(d*i))
+                }
+                i++
+            }
+        }
+        d++
+    }
+    return factors.size
 }
