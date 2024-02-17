fbfafcddc445cf0b1de2af47e4ec6f5754100128
Kevin Crimi <kevincrimi@gmail.com>
2024-02-16 16:57:01 -0800

    Update Solution.ts

--- Java/src/Codility/MaxProductOfThree/Solution.ts
+++ Java/src/Codility/MaxProductOfThree/Solution.ts
@@ -1,5 +1,5 @@
 function solution(A: number[]): number {
-    A.sort()
+    A.sort((a,b)=> a-b)
     return Math.max(A[A.length-1] * A[A.length-2] * A[A.length-3],
         A[0] * A[1] * A[A.length-1])
 
