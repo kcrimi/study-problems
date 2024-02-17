c9dcb4dac6d4b08c5abe61ebafc8f6a0d4b298d7
Kevin Crimi <kevincrimi@gmail.com>
2024-02-17 14:29:15 -0800

    Update Solution.ts

--- Java/src/Codility/MinMaxDivision/Solution.ts
+++ Java/src/Codility/MinMaxDivision/Solution.ts
@@ -3,11 +3,11 @@
 
 function solution(K: number, M: number, A: number[]): number {
     var upperBound = M * A.length
-    var lowerBound = 0
+    var lowerBound = Math.min(...A)
     var minLargeSum = upperBound
+    // console.log ({upperBound, lowerBound, minLargeSum})
     while (upperBound > lowerBound) {
-        const midpoint = Math.ceil((upperBound + lowerBound) / 2)
-        // console.log ({upperBound, lowerBound, midpoint})
+        const midpoint = Math.floor((upperBound + lowerBound) / 2)
         // Check if can make make true with K blocks
         var blocks = 1
         var currentBlock = 0
@@ -15,22 +15,19 @@ function solution(K: number, M: number, A: number[]): number {
         for (let i = 0; i < A.length; i++) {
             if (currentBlock + A[i] > midpoint){
                 blocks++
-                if (blocks > K || A[i] > midpoint) {
-                    // sum too low, need too many blocks
-                    lowerBound = midpoint +1
-                    tooLow = true
-                    break
-                }
                 currentBlock = A[i]
             } else {
                 currentBlock += A[i]
             }
         }
-        if (!tooLow && blocks <= K) { 
+        if (blocks > K) {
+            // sum too low, need too many blocks
+            lowerBound = midpoint +1
+        } else { 
             minLargeSum = midpoint
-            upperBound = midpoint-1
+            upperBound = midpoint - 1
         }
-        // console.log ({upperBound, lowerBound, midpoint})
+        // console.log ({upperBound, lowerBound, midpoint, minLargeSum})
     }
     return minLargeSum
 }
