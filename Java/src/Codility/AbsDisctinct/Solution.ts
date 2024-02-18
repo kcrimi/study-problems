71cdcaa17209ecfb2c7f8fa5bff27940a35948ed
Kevin Crimi <kevincrimi@gmail.com>
2024-02-17 16:45:12 -0800

    Update Solution.ts

--- Java/src/Codility/AbsDisctinct/Solution.ts
+++ Java/src/Codility/AbsDisctinct/Solution.ts
@@ -4,3 +4,24 @@ function solution(A: number[]): number {
     const set = new Set(absA)
     return set.size
 }
+
+//Caterpillar as intended?
+function solution(A: number[]): number {
+    var left = 0
+    var right = A.length -1
+    var current = Math.max(Math.abs(A[left]), Math.abs(A[right]))
+    var distinct = 1
+    while (left < right) {
+        if (Math.abs(A[left]) == current) {
+            left++
+        } 
+        if(Math.abs(A[right]) == current) {
+            right--
+        } 
+        if(Math.abs(A[left]) != current && Math.abs(A[right]) != current) {
+            current = Math.max(Math.abs(A[left]), Math.abs(A[right]))
+            distinct++
+        }
+    }
+    return distinct
+}
