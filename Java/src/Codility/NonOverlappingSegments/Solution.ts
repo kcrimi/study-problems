b3d8720faa566931157241c70ce9e5702e52a8e2
Kevin Crimi <kevincrimi@gmail.com>
2024-02-17 16:56:48 -0800

    Create Solution.ts

---
+++ Java/src/Codility/NonOverlappingSegments/Solution.ts
@@ -0,0 +1,17 @@
+function solution(A: number[], B: number[]): number {
+    var maxSegments = 0
+    var first = 0
+    while (first + maxSegments < A.length) {
+        var currentEnd = -1
+        var segments = 0
+        for (let i = first; i < A.length; i++) {
+            if (A[i] > currentEnd) {
+                segments++
+                currentEnd = B[i]
+            }
+        }
+        maxSegments = Math.max(maxSegments, segments)
+        first++
+    }
+    return maxSegments
+}
