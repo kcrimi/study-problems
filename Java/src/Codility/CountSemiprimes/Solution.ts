6e105645ba1a413b234d0746b935b099e0a0d037
Kevin Crimi <kevincrimi@gmail.com>
2024-02-18 22:38:10 -0800

    Update Solution.ts

--- Java/src/Codility/CountSemiprimes/Solution.ts
+++ Java/src/Codility/CountSemiprimes/Solution.ts
@@ -1,5 +1,3 @@
-// one wrong, timeing out long
-
 function solution(N: number, P: number[], Q: number[]): number[] {
     //Find available primes
     const possibleNums = Array.from(
@@ -7,56 +5,17 @@ function solution(N: number, P: number[], Q: number[]): number[] {
         (_, i) => i+1
     )
     const primes = new Set(possibleNums)
+    // console.log(primes)
     primes.delete(1)
-    for (let i = 2; i <= Math.sqrt(N); i++){
+    for (let i = 2; i < Math.sqrt(N); i++){
         if (primes.has(i)) {
             for (let j = 2; j <= N / i; j++){
                 primes.delete(i*j)
             }
         }
     }
-    const primesArray = new Array<number>(...primes)
-    // console.log(primesArray)
-
-    //Find semiprimes
-    const semiPrimes = new Set<number>()
-    primesArray.forEach((valA, i) => {
-        for (let j = i; j < primesArray.length; j++){
-            const semiPrime = valA * primesArray[j]
-            if (semiPrime > N) break
-            semiPrimes.add(valA * primesArray[j])
-        }
-    })
-    const semiPrimesArray = new Array(...semiPrimes)
-    // console.log(semiPrimesArray)
-
-    //Run Queries
-    return P.map((start, i)=>{
-        const end = Q[i]
-        return semiPrimesArray.reduce((prev, val) => {
-            if (val >= start && val <= end) prev++
-            return prev
-        },0)
-    })
-}
-
-// failing one test
-function solution(N: number, P: number[], Q: number[]): number[] {
-    //Find available primes
-    const possibleNums = Array.from(
-        {length:N/2},
-        (_, i) => i+1
-    )
-    const primes = new Set(possibleNums)
-    primes.delete(1)
-    for (let i = 2; i <= Math.sqrt(N); i++){
-        if (primes.has(i)) {
-            for (let j = 2; j <= N / i; j++){
-                primes.delete(i*j)
-            }
-        }
-    }
-    const primesArray = new Array<number>(...primes)
+    // console.log(primes)
+    const primesArray = Array.from(primes)
     // console.log(primesArray)
 
     //Find semiprimes
@@ -67,7 +26,7 @@ function solution(N: number, P: number[], Q: number[]): number[] {
             semiPrimes.add(valA * primesArray[j])
         }
     }
-    const semiPrimesArray = new Array(...semiPrimes).sort((a,b)=> a-b)
+    const semiPrimesArray =  Array.from(semiPrimes).sort((a,b)=> a-b)
     // console.log(semiPrimesArray)
     
     //Run Queries
