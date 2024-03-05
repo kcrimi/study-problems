2345d77b181eda2a970d7c9fbb9124f8b25350a5
Kevin Crimi <kevincrimi@gmail.com>
2024-03-04 17:08:12 -0800

    Update Solution.js

--- interviewBit/SortByColor/Solution.js
+++ interviewBit/SortByColor/Solution.js
@@ -15,3 +15,30 @@ module.exports = {
 		return output
 	}
 };
+
+// Intended 2 pointer implementation
+module.exports = { 
+ //param A : array of integers
+ //return a array of integers
+	sortColors : function(A){
+		let head = 0
+		let mid = 0
+		let tail = A.length -1
+		while (mid <= tail) {
+			if (A[mid] == 1) {
+				mid++
+			} else if (A[mid] == 0) {
+				A[mid] = A[head]
+				A[head] = 0
+				head++
+				mid++
+			} else if (A[mid] == 2) {
+				A[mid] = A[tail]
+				A[tail] = 2
+				tail--
+			}
+			// console.log(A.slice(0,11))
+		}
+		return A
+	}
+};
