4dab0f03678c7f9b32d1c53f8aa6a5b2bc47c253
Kevin Crimi <kevincrimi@gmail.com>
2024-03-08 11:36:33 -0800

    Create Solution.ts

---
+++ HackerRank/DesignerPdfViewer/Solution.ts
@@ -0,0 +1,54 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'designerPdfViewer' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY h
+ *  2. STRING word
+ */
+
+function designerPdfViewer(h: number[], word: string): number {
+    const chars = word.split("")
+    const heights = chars.map((char) => h[char.charCodeAt(0) - 97])
+    const maxHeight = Math.max(...heights)
+    return maxHeight * heights.length
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const h: number[] = readLine().replace(/\s+$/g, '').split(' ').map(hTemp => parseInt(hTemp, 10));
+
+    const word: string = readLine();
+
+    const result: number = designerPdfViewer(h, word);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
